4077aba8f259be53143b4aabbbbd88d30ba0e9c0
bradministrator <bradministrator@users.noreply.github.com>
2017-02-17 16:49:00 -0600

    Update prototype_state_observer.js

--- prototype_state_observer.js
+++ prototype_state_observer.js
@@ -64,6 +64,9 @@ var LoanRespondent = function(){
     	for(i = 0; i < this.observers.length; i++){
     		this.observers[i].update(subject);
     	}
+    },
+    handleInput(){
+    	this.currentState.handleInput();
     }
 	}
 
@@ -72,70 +75,61 @@ var LoanRespondent = function(){
 	}
 
 	MyObserver.prototype = {
-		update: function(thing){
-			thing.currentState.handleInput();
+		update: function(observed){
+			observed.handleInput();
 		}
 	}
 
 	var UnknownState = function(respondent){
-		this.respondent = respondent;
-
 		this.handleInput = function(){
 			console.log('UnknownState Handling input');
-			var currentState = this.respondent.qualifies();
+			var currentState = respondent.qualifies();
 			if(currentState){
 				console.log('changing to Qualified from Unknown');
-				this.respondent.change(new QualifiedState(this.respondent));
+				respondent.change(new QualifiedState(respondent));
 			}else	if(currentState == false){
 				console.log('changing to Unqualified from Unknown');
-				this.respondent.change(new UnqualifiedState(this.respondent));
+				respondent.change(new UnqualifiedState(respondent));
 			}
 		}
 
 		this.enter = function(){
-			//respondent.change(new Qualified(respondent));
 			console.log('changed to:  Unknown, setting up...');
 		}
 	}
 
 	var QualifiedState = function(respondent){
-		this.respondent = respondent;
-
 		this.handleInput = function(){
 			console.log('QualifiedState Handling input');
-			var currentState = this.respondent.qualifies();
+			var currentState = respondent.qualifies();
 			if(currentState == undefined){
 				console.log('changing to Unknown from Qualified');
-				this.respondent.change(new UnknownState(this.respondent));
+				respondent.change(new UnknownState(respondent));
 			}else	if(currentState == false){
 				console.log('changing to Unqualified from Qualified');
-				this.respondent.change(new UnqualifiedState(this.respondent));
+				respondent.change(new UnqualifiedState(respondent));
 			}
 		}
 
 		this.enter = function(){
-			//respondent.change(new NotQualified(respondent));
 			console.log('changed to:  Qualified, setting up...');
 		}
 	}
 
 	var UnqualifiedState = function(respondent){
-		this.respondent = respondent;
-
 		this.handleInput = function(){
 			console.log('UnqualifiedState Handling input');
-			var currentState = this.respondent.qualifies();
+			var currentState = respondent.qualifies();
 			if(currentState){
 				console.log('changing to Qualified from Unqualified');
-				this.respondent.change(new QualifiedState(this.respondent));
+				respondent.change(new QualifiedState(respondent));
 			}else	if(currentState == undefined){
 				console.log('changing to Unknown from Unqualified');
-				this.respondent.change(new UnknownState(this.respondent));
+				respondent.change(new UnknownState(respondent));
 			}
 		}
 
 		this.enter = function(){
-			//respondent.change(new UnknownState(respondent));
 			console.log('changed to:  UnqualifiedState, setting up...');
 		}
 	}
